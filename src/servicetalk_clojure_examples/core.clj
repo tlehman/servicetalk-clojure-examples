db04d9246c558a00eea51a359b0f64528feebbcc
Tobi Lehman <mail@tobilehman.com>
2023-07-09 09:51:38 -0700

    Remove main

--- src/servicetalk_clojure_examples/core.clj
+++ src/servicetalk_clojure_examples/core.clj
@@ -1,16 +1,11 @@
 (ns servicetalk-clojure-examples.core
-  (:import (io.servicetalk.http.api DefaultHttpExecutionStrategy BlockingHttpService HttpSerializers))
-  (:import (io.servicetalk.http.netty HttpServers))
-  (:gen-class))
-nil
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  (:import (io.servicetalk.http.api BlockingHttpService
+                                    DefaultHttpExecutionStrategy
+                                    HttpSerializers))
+  (:import (io.servicetalk.http.netty HttpServers)))
+
 
 ;; https://github.com/apple/servicetalk/blob/main/servicetalk-examples/http/helloworld/src/main/java/io/servicetalk/examples/http/helloworld/blocking/BlockingHelloWorldServer.java
-
-
 (let [http-server-builder (HttpServers/forPort 8080)
       serializer (HttpSerializers/textSerializerUtf8)
       handler (proxy [BlockingHttpService][]
@@ -28,12 +23,15 @@ nil
 
 
 
+
 (fn [ctx req resFactory]
   (-> resFactory
       (.ok)
       (.payloadBody "Hello, world!!" serializer)))
 
-;DefaultHttpExecutionStrategy/OFFLOAD_RECEIVE_DATA_STRATEGY
+DefaultHttpExecutionStrategy/OFFLOAD_RECEIVE_DATA_STRATEGY
+
 ;io.servicetalk.http.api.DefaultHttpExecutionStrategy
+
 
 
